7109f19642121f4a102880eb7cc3e8504489585f
PedroReynoldsBrandao <88774646+PedroReynoldsBrandao@users.noreply.github.com>
2024-10-01 11:41:25 +0100

    Update aux_functions.py

--- Scripts for plotting and modelling/aux_functions.py
+++ Scripts for plotting and modelling/aux_functions.py
@@ -51,6 +51,24 @@ def generate_noise(std,shift,numofsamples):
     return np.random.normal(0, std*shift, numofsamples)
 
 def augment_df_byday(df,numofreps,shift,bioparam):
+
+    """
+    Like any experimental procedure, standard analytical methods are susceptible to human error. 
+    However, in multi-step processes such as HPLC and cytometry, the potential for error propagation is significant. 
+    To deal with this, technical replicates are recommended, but when combined with biological replicates they can excessively increase the total labor and expense. 
+    In this work, data augmentation was used to simulate technical replication. Data augmentation is a statistical technique popularly used in machine learning,
+    and it provides the ability to simulate an expected form of irrelevant variance in a data set, improving its quality.
+    
+    In this case, the irrelevant variance to mitigate is human experimental error on the standard analytical methods side.
+    
+    For this purpose, a 5-fold data augmentation was applied, following these steps:
+    1)	Separately collect the data on the Day of each assay (e.g, Day 3 of assay F/2, Day 5 of assay F/2, …, Day 20 of assay F/2+N+P)
+    2)	For each, generate normal distributions for fucoxanthin and cell count, based on their individual mean and standard deviation
+    3)	Generate 4 artificial technical replicates for each assay’s Day, by random sampling of the generated distribution
+    4)	Create 4 new observations per assay/Day, using the new values of Fx and CC, while just repeating the spectroscopy values
+    The data augmentation process was only applied for the training subset.
+
+    """
     
     
     augmented_data = pd.DataFrame(columns=df.columns,data=df.values,index = df.index)
